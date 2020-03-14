5ff3c11bee81514d7023ad294f4a6169abfb6201
Esmilis Norvaisas <esmilis.norvaisas@railsbank.com>
2020-03-14 17:36:52 +0200

    follow up

--- index.js
+++ index.js
@@ -1,102 +1,130 @@
-const { Formatter, SummaryFormatter, formatterHelpers } = require('cucumber');
+const {Formatter, formatterHelpers} = require('cucumber');
+const axios = require('axios');
+const Rxjs = require('rxjs');
 
 class CucumberjsSlackFormatter extends Formatter {
+
+    state = new Rxjs.Subject();
+
+
     /** @param {Options} options */
     constructor(options) {
         super(options);
-        this.colorsEnabled = options.colorsEnabled;
-        this.descriptionEnabled = options.descriptionEnabled;
+
+        this.updateSlack(options.slackReporterOptions, this.buildState);
 
         options.eventBroadcaster.on('test-case-started', (event) => {
-            const { gherkinDocument, pickle } = options.eventDataCollector.getTestCaseAttempt(event);
+            const {pickle} = options.eventDataCollector.getTestCaseAttempt(event);
 
-            this.log("case started", )
-
-
-            if (this.uri !== event.sourceLocation.uri) {
-                const { feature } = gherkinDocument;
-
-                if (this.uri) this.logn();
-
-                const tags = feature.tags.map((tag) => tag.name).join(' ');
-                if (tags) this.logn(options.colorFns.tag(tags));
-
-                this.logn(`${feature.keyword}: ${feature.name}`);
-
-                if (feature.description && this.descriptionEnabled) this.logn(`${feature.description}`);
-
-                this.uri = event.sourceLocation.uri;
-            }
-            //
-            // this.logn();
-            //
-            const tags = pickle.tags.map((tag) => tag.name).join(' ');
-            if (tags) this.logn(tags, 2);
-            //
-            const line = Math.min(...pickle.locations.map((location) => location.line));
-            const { keyword } = gherkinDocument.feature.children.find((child) => child.location.line === line);
-            //
-            this.logn(`${keyword}: ${pickle.name}`, 2);
+            this.startCase(pickle.name, pickle.steps.map(step => ({text: step.text})));
         });
 
         options.eventBroadcaster.on('test-step-started', (event) => {
-            const testCaseAttempt = options.eventDataCollector.getTestCaseAttempt(event.testCase);
+            const testStep = stepFromEvent(options, event);
+            if (!testStep.sourceLocation) return; // skip
 
-            this.log("step started", JSON.stringify(testCaseAttempt,null, 4))
-            // testCaseAttempt.stepResults = testCaseAttempt.testCase.steps.map(() => ({}));
-            //
-            // const testStep = formatterHelpers.parseTestCaseAttempt({ testCaseAttempt }).testSteps[event.index];
-            // if (!testStep.sourceLocation) return; // hook
-            //
-            // this.logn(`${this.color(testStep.keyword.trim(), 'bold')} ${testStep.text}`, 4);
-            //
-            // testStep.arguments.forEach((argument) => {
-            //     if (argument.content) {
-            //         this.logn(`"""${n}${argument.content}${n}"""`, 6);
-            //     }
-            //
-            //     if (argument.rows) {
-            //         const datatable = new Table(table);
-            //         datatable.push(...argument.rows);
-            //         this.logn(datatable, 6);
-            //     }
-            // });
+            this.startStep(testStep.text);
         });
 
         options.eventBroadcaster.on('test-step-finished', (event) => {
-            const { result: { status, exception } } = event;
+            const testStep = stepFromEvent(options, event);
 
-            // if (status !== 'passed') {
-            //     this.logn(options.colorFns[status](`${marks[status]} ${status}`), 4);
-            // }
-            //
-            // if (exception) {
-            //     const error = formatterHelpers.formatError(exception, options.colorFns);
-            //     this.logn(error, 6);
-            // }
-            this.log("finished ", JSON.stringify(status), JSON.stringify(exception))
+            if (!testStep.sourceLocation) return; //skip
 
-        });
+            const {result: {status, exception}} = event;
 
-        options.eventBroadcaster.on('test-run-finished', (event) => {
-            const noptions = Object.create(options, { eventBroadcaster: { value: { on: () => { } } } });
-            // const formatter = new SummaryFormatter(noptions);
-            // if (this.uri) this.logn();
-            // formatter.logSummary(event);
-            this.log("sup")
-
+            this.endStep(testStep.text, status, exception)
         });
     }
 
-    // color(value, ...color) {
-    //     return this.colorsEnabled ? color.reduce((v, c) => v[c], colors)(value) : value;
-    // }
+    startCase(name, steps) {
+        console.log('case', name, steps);
+    }
 
-    logn(value = '', indent = 0) {
-        let text = value.toString();
-        if (indent > 0) text = text.replace(/^/gm, ' '.repeat(indent));
-        this.log(`${text}`);
+    startStep(name) {
+        console.log('startstep', name);
+    }
+
+    endStep(name, status, exception){
+        console.log('stopstep', name, status, exception);
+    }
+
+    //failed cases with failed step
+    //current case, current step
+
+    //summary
+    //case count
+    //cases passed
+    //cases failed
+
+    //feature
+    //tags
+    //environment
+    //triggered by person
+    //full report uploaded here
+    ts;
+
+
+    getBody(buildState, options) {
+       return [
+           {
+               "type": "section",
+               "text": {
+                   "type": "mrkdwn",
+                   "text": `Release run: <http://www|#12345>running *"${options.tags}"* | 12 :arrow_forward:  3 :white_check_mark:  0 :x:`
+               }
+           },
+           {
+               "type": "divider"
+           },
+           {
+               "type": "section",
+               "text": {
+                   "type": "mrkdwn",
+                   "text": `*${buildState.current.name}* \n${buildState.current.steps.map(step => step.state).join(' ')}\n - step: ${buildState.current.steps.find(step => step.inProgress).name}`
+               }
+           },
+           {
+               "type": "divider"
+           },
+           {
+               "type": "section",
+               "text": {
+                   "type": "mrkdwn",
+                   "text": `Environment *${options.environment}*, triggered by *${options.initiator}*, Full report <http://www|here>`
+               }
+           }
+       ]
+    }
+
+
+    async updateSlack(options,state) {
+        // if (!this.ts) {
+        //     const response = await axios.post('https://slack.com/api/chat.postMessage', {
+        //         channel: options.channel, text: "Starting test run", blocks: this.getBody(state, options)
+        //     }, {
+        //         headers: {"Authorization": options.authorization, "Content-type": "application/json"}
+        //     })
+        //     this.ts = response.data.ts;
+        // } else {
+        //     const response = await axios.post('https://slack.com/api/chat.update', {
+        //         channel: options.channel,
+        //         text: "Starting test run",
+        //         blocks: this.getBody(this.buildState, options),
+        //         ts: this.ts
+        //     }, {
+        //         headers: {"Authorization": options.authorization, "Content-type": "application/json"}
+        //     })
+        // }
     }
 }
 
 module.exports = CucumberjsSlackFormatter;
+
+function stepFromEvent(options, event) {
+    const testCaseAttempt = options.eventDataCollector.getTestCaseAttempt(event.testCase);
+
+    testCaseAttempt.stepResults = testCaseAttempt.testCase.steps.map(() => ({}));
+    const testStep = formatterHelpers.parseTestCaseAttempt({testCaseAttempt}).testSteps[event.index];
+    return testStep;
+}
